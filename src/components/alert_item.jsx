8c75474111bce6be83e3f648c35b999795da93c8
Victor Hugo <vh.monteiro@gmail.com>
2017-01-16 14:11:17 -0200

    CSS Widget OK, Warning and Critical.

--- src/components/alert_item.jsx
+++ src/components/alert_item.jsx
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 
 const AlertItem = () => {
     return (
-        <div className="dash-alert-item">
+        <div>
+        <div className="dash-alert-item critical">
             <span className="title">CI: Vault</span>
             <strong className="value">Build Fail</strong>
+        </div>
+        <div className="dash-alert-item warning">
+            <span className="title">CI: FaaS</span>
+            <strong className="value">Build Stoped</strong>
+        </div>
+        <div className="dash-alert-item ok">
+            <span className="title">CI: FaaS</span>
+            <strong className="value">Build OK</strong>
+        </div>
         </div>
     );
 }
