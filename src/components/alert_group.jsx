37e970e1a03c480301f2bafbeb4fa5b9194a652e
Alan <avanselmo@gmail.com>
2017-01-16 15:07:12 -0200

    alert group component

---
+++ src/components/alert_group.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class AlertGroup extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="dash-alert-group"></div>
+    )
+  }
+
+}
+
+export default AlertGroup;
