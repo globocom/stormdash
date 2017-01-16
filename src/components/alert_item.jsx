34096303f973f09d912726b2a48b17ac47f988d0
Alan <avanselmo@gmail.com>
2017-01-16 14:59:31 -0200

    Some alert item fixes

--- src/components/alert_item.jsx
+++ src/components/alert_item.jsx
@@ -4,15 +4,15 @@ const AlertItem = () => {
     return (
         <div>
         <div className="dash-alert-item critical">
-            <span className="title">CI: Vault</span>
+            <span className="title">CI: Vault</span><br />
             <strong className="value">Build Fail</strong>
         </div>
         <div className="dash-alert-item warning">
-            <span className="title">CI: FaaS</span>
+            <span className="title">CI: FaaS</span><br />
             <strong className="value">Build Stoped</strong>
         </div>
         <div className="dash-alert-item ok">
-            <span className="title">CI: FaaS</span>
+            <span className="title">CI: FaaS</span><br />
             <strong className="value">Build OK</strong>
         </div>
         </div>
