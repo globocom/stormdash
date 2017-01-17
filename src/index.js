020a413d408f270800f5eebabfec0b19d4e93a05
Alan <avanselmo@gmail.com>
2017-01-17 15:09:40 -0200

    Add topcoat css to index.html

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 
-import '../style/normalize.css';
 import '../style/base.css';
 
 ReactDOM.render(<App />, document.querySelector('.container'));
