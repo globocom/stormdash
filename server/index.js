17caea46ec90398c1b71c6f572b8ef58855ea377
Alan <avanselmo@gmail.com>
2017-02-23 19:29:06 -0300

    Add uws (uWebSocket)

--- server/index.js
+++ server/index.js
@@ -1,9 +1,13 @@
 const app = require('./app');
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const httpServer = require('http').Server(app);
+const socketIO = require('socket.io');
+const uws = require('uws');
+const io = socketIO(httpServer);
 const IOServer = require('./IOServer');
 
+io.ws = new uws.Server({ perMessageDeflate: false });
 global.ioserver = new IOServer(io);
+
 const PORT = process.env.PORT || 8888;
 
 http.listen(PORT, '0.0.0.0', () => {
