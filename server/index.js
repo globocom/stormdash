bcec33ba2dd7ac49457b8b572de6860d6c025a4a
Alan <alan.vitor@corp.globo.com>
2017-12-07 11:22:12 -0200

    Remove uws

--- server/index.js
+++ server/index.js
@@ -17,11 +17,9 @@ limitations under the License.
 const app = require('./app');
 const httpServer = require('http').Server(app);
 const socketIO = require('socket.io');
-const uws = require('uws');
 const io = socketIO(httpServer);
 const IOServer = require('./IOServer');
 
-io.ws = new uws.Server({ perMessageDeflate: false });
 global.ioserver = new IOServer(io);
 
 const PORT = process.env.PORT || 8888;
